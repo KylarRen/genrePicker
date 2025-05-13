829ce970ea5daaaed65da71e18a1cba7a0ab1fea
KylarRen <70128187+KylarRen@users.noreply.github.com>
2025-05-13 00:16:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-var genreList = import('./genres.txt');
-
-//console.log(genreList.split('\n'));
+var genreList = genreListString.split('\n');
 
 function getNewGenre(){
 
-    console.log("you clicked");
+    var displayDiv = document.getElementById("currentGenre");
+    var tempGenre = genreList[getRandomInt(0, genreList.length)];
+
+    console.log(tempGenre);
+
+    displayDiv.innerHTML = tempGenre;
 
 }
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getNewGenre();
