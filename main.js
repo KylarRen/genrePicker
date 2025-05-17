3d6f8617e6ef6fe5b7661692662262988d7c75aa
KylarRen <70128187+KylarRen@users.noreply.github.com>
2025-05-17 18:51:48 +0000

    added potential to add imagees to the genres

--- main.js
+++ main.js
@@ -4,16 +4,27 @@ function getNewGenre(){
 
     var displayDiv = document.getElementById("currentGenre");
     var tempGenre = genreList[getRandomInt(0, genreList.length)];
+    var tempGenreShort = tempGenre.split(/-|–/)[0].trim();
 
-    console.log(tempGenre);
+    console.log(tempGenreShort);
 
     displayDiv.innerHTML = tempGenre;
 
+    /*
+
+    var img = document.getElementById("genreImage");
+    img.src = "https://unsplash.com/800x400/?" + encodeURIComponent(tempGenreShort + " music");
+    console.log(img.src);
+
+    */
+
 }
+
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
+    
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+    
 }
 
 getNewGenre();
